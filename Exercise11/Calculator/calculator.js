ec5ade6519d018bf17c69c9a11731e4c10abe4f3
Haribiddacdw <hariprasath.viswanathan@cdw.com>
2023-03-16 19:47:01 +0530

    Updated code
    added docs

--- Exercise11/Calculator/calculator.js
+++ Exercise11/Calculator/calculator.js
@@ -1,14 +1,38 @@
 
 const Calculator = {
+  /** 
+   * function addition it will add two numbers
+   * @params num1 
+   * @params num2
+   * @return addition result 
+  */
   add: function(num1, num2) {
     return num1 + num2;
   },
+  /** 
+   * function subtract it will subtract two numbers
+   * @params num1 
+   * @params num2
+   * @return subtraction result 
+  */
   subtract: function(num1, num2) {
     return num1 - num2;
   },
+  /** 
+   * function multiply it will multiply two numbers
+   * @params num1 
+   * @params num2
+   * @return multiplication result 
+  */
   multiply: function(num1, num2) {
     return num1 * num2;
   },
+   /** 
+   * function division it will divide two numbers
+   * @params num1 
+   * @params num2
+   * @return division result 
+  */
   divide: function(num1, num2) {
     if (num2 === 0) {
       return "Error: Division by zero";
@@ -17,7 +41,9 @@ const Calculator = {
   }
 };
 
+//variable declaration
 let num1, num2;
+//get the user input
 do {
   num1 = parseInt(prompt("Enter the first number:"));
 } while (!Number.isInteger(num1));
@@ -28,6 +54,7 @@ const operation = prompt("Choose an operation (+, -, *, /):");
 
 
 let result;
+//function call based on user selection
 switch(operation) {
   case "+":
     result = Calculator.add(num1, num2);
